fddf2bda8bb592365db52b7097ae804d912caf28
Dat Truong <dat.truonghuu1212@gmail.com>
2018-01-27 17:13:30 +0200

    Color-filter-no-autocontrast

--- imageEditor/src/app/color-filter/color-filter.component.ts
+++ imageEditor/src/app/color-filter/color-filter.component.ts
@@ -1,24 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ImageService} from '../services/image.service';
 
 @Component({
   selector: 'app-color-filter',
   templateUrl: './color-filter.component.html',
-  styleUrls: ['./color-filter.component.css']
+  styleUrls: ['./color-filter.component.css'],
 })
 export class ColorFilterComponent implements OnInit {
-  private red = 0.2126;
-  private green = 0.7152;
-  private blue = 0.0722;
-  private strength = 0;
+  private red: any = 0.2126;
+  private green: any = 0.7152;
+  private blue: any = 0.0722;
+  private strength: any = 0;
 
   constructor(private imageService: ImageService) { }
 
-  private colorFilter =() => {
+  private colorFilter = () => {
+    for (let i = 0; i < this.imageService.numPixels; i++) {
+      this.imageService.pixels[i * 4] = this.imageService.pixels[i * 4] +
+        this.red % (101 - this.strength);
+      this.imageService.pixels[i * 4] = this.imageService.pixels[i * 4] +
+        this.green % (101 - this.strength);
+      this.imageService.pixels[i * 4] = this.imageService.pixels[i * 4] +
+        this.blue % (101 - this.strength);
+    }
 
-  }
+    this.imageService.context.clearRect(0, 0, this.imageService.canvas.width,
+      this.imageService.canvas.height);
+    this.imageService.context.putImageData(this.imageService.imageData, 0, 0);
+  };
 
   ngOnInit() {
+    this.imageService.functions.colorFilter = this.colorFilter;
   }
 
 }
